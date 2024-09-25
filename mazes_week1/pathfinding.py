960cbb4f66f07131b6cd93f2387d9a8102abb082
EwanWilliams <ewan@ewan.name>
2024-09-25 11:48:15 +0100

    pathfinding.py started implementation

--- mazes_week1/pathfinding.py
+++ mazes_week1/pathfinding.py
@@ -3,9 +3,8 @@ import heapq
 
 
 def a_star(maze, start, goal):
-    # Create a list that will act as the priority queue and using heapq.heappush - add the starting
-    # node to the queue with a priority of 0
-    queue = 
+    p_queue = []
+    heapq.heappush(p_queue, (0, start))
 
     # Proving some code for you as they will be helpful shortly
     directions = {
@@ -14,15 +13,14 @@ def a_star(maze, start, goal):
         "up": (-1, 0),
         "down": (1, 0)
     }
-    predecessors = {start: None}    # Enables the get_path function to backtrack
-    g_values = {start: 0}   # The g score for each cell
+    predecessors = {start: None} # Enables the get_path function to backtrack
+    g_values = {start: 0} # The g score for each cell
 
-    # loop through the queue, this is an infinite loop that will only stop if the queue is empty
-        current_cell = # get this from the priority queue
+    while len(p_queue) > 0: # loop through the queue, this is an infinite loop that will only stop if the queue is empty
+        current_cell = heapq.heappop(p_queue)[1] # get this from the priority queue
 
-        # Check if the current cell is the goal:
-            # if it is, run this command:
-            # return get_path(predecessors, start, goal)
+        if current_cell == goal: # Check if the current cell is the goal:
+            return get_path(predecessors, start, goal)
 
         # Now lets look at where we can move to from the current cell.
         # For each direction do the following:
@@ -70,6 +68,6 @@ def get_path(predecessors, start, goal):
 if __name__ == "__main__":
     maze_map = utils.import_maze("mazes/maze1.txt")     # Change the path as required.
     start = utils.locate(maze_map, 's')
-    # Looking at the line above, do the same thing but this time, locate the goal
+    goal = utils.locate(maze_map, 'g')
     # Print out the path returned by the a_star function (after you have completed it)
     print(a_star(maze_map, start, goal))
